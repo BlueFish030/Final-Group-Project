b174a10ceb30e781ef449455e4e5975ffbf5e768
desktop-tutorial <ken0neth0330@gmail.com>
2023-03-01 21:47:15 +0800

    more update by kenneth

--- gameboy/src/Game3pic.js
+++ gameboy/src/Game3pic.js
@@ -4,7 +4,7 @@ import Game3menu from './img/game3menu.gif';
 export default function Game3pic() {
   return (
     <div>
-        <img id='calImg' src={Game3menu} style={{width:"70px",}} alt="Game3 picture" />
+        <img id='calImg' src={Game3menu} style={{width:"60px",}} alt="Game3 picture" />
     </div>
 
   )
