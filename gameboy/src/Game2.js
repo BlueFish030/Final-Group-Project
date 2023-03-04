0b39c4458dcf6b7aea742dace09976a8cfe5f0c1
Limi66 <limih19@gmail.com>
2023-03-04 20:36:29 +0800

    add userBalance function and revise css of game2

--- gameboy/src/Game2.js
+++ gameboy/src/Game2.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //https://fontawesome.com/v5/docs/web/use-with/react
 import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons';
+import arrowImg from './arrow-1027_128.gif';
 
 function Game2() {
   const [userBet, setUserBet] = useState('');
   const [computerRolls, setComputerRolls] = useState([]);
   const [roundOutcome, setRoundOutcome] = useState('');
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [userBalance, setUserBalance] = useState(100);
+  const [betAmount, setBetAmount] = useState(10);
 
   function handleBack() {
     const menu = document.getElementById('menu');
@@ -27,6 +30,7 @@ function Game2() {
     ];
   }
 
+
   function handleBet(betType) {
     setUserBet(betType);
     setButtonDisabled(true);
@@ -38,14 +42,18 @@ function Game2() {
     if (betType === 'small') {
       if (total < 11) {
         setRoundOutcome('You win!'); //Small (total 3-10)
+        setUserBalance(userBalance + betAmount);
       } else {
         setRoundOutcome('Computer wins!');
+        setUserBalance(userBalance - betAmount);
       }
     } else {
       if (total > 10) {
         setRoundOutcome('You win!'); //Big (total 11-18)
+        setUserBalance(userBalance + betAmount);
       } else {
         setRoundOutcome('Computer wins!');
+        setUserBalance(userBalance - betAmount);
       }
     }
   }
@@ -55,6 +63,7 @@ function Game2() {
     setComputerRolls([]);
     setRoundOutcome('');
     setButtonDisabled(false);
+    setBetAmount(10);
   }
 
   function renderDice(roll) {
@@ -62,42 +71,42 @@ function Game2() {
       case 1:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceOne} />
+            <FontAwesomeIcon icon={faDiceOne} className='diceIcon'/>
             {' '}
           </>
         );
       case 2:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceTwo} />
+            <FontAwesomeIcon icon={faDiceTwo} className='diceIcon'/>
             {' '}
           </>
         );
       case 3:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceThree} />
+            <FontAwesomeIcon icon={faDiceThree} className='diceIcon'/>
             {' '}
           </>
         );
       case 4:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceFour} />
+            <FontAwesomeIcon icon={faDiceFour} className='diceIcon'/>
             {' '}
           </>
         );
       case 5:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceFive} />
+            <FontAwesomeIcon icon={faDiceFive} className='diceIcon'/>
             {' '}
           </>
         );
       case 6:
         return (
           <>
-            <FontAwesomeIcon icon={faDiceSix} />
+            <FontAwesomeIcon icon={faDiceSix} className='diceIcon'/>
             {' '}
           </>
         );
@@ -106,18 +115,45 @@ function Game2() {
     }
   }
 
+  function handleBetAmountChange(event) {
+    const newAmount = parseInt(event.target.value);
+    if (!isNaN(newAmount)) {
+      //set the amount should be between 0 and the max of userBalance
+      setBetAmount(Math.max(0, Math.min(newAmount, userBalance))); 
+    } else {
+      setBetAmount('');
+    }
+  }
+
   return (
     <div className='Game2Container' id='g2Container'>
       <h1>Sic Bo</h1>
-      <p>Select your bet:</p>
-      <button disabled={buttonDisabled} onClick={() => handleBet('small')}>
-        Small 
-      </button>
-      <button disabled={buttonDisabled} onClick={() => handleBet('big')}>
-        Big
-      </button>
-      {userBet && (
+      <div id='g2c1'>
         <div>
+          <p>Your Balance: {userBalance}</p>
+        </div>
+        <div>
+          <label htmlFor="betAmountInput">Bet amount: </label>
+          <input
+            id="betAmountInput"
+            type="number"
+            value={betAmount}
+            onChange={handleBetAmountChange}
+          />
+        </div>
+      </div>
+      <div id='g2c2'>
+        <p> Select your bet </p>
+        <img id='arrowImg' src={arrowImg} alt="arrow" />
+        <button disabled={buttonDisabled} onClick={() => handleBet('small')}>
+          Small 
+        </button>
+        <button disabled={buttonDisabled} onClick={() => handleBet('big')}>
+          Big
+        </button>
+      </div>
+      {userBet && (
+        <div id='g2c3'>
           <p>Your bet: {userBet}</p>
           <p>
             Dice rolls:{' '}
